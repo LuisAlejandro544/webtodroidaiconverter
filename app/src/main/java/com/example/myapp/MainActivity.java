6c800e00dae5164d88b5eaea72aa7bf8b0050513
LuisAlejandro544 <luisalejandrososacamacho9@gmail.com>
2026-01-05 18:11:12 -0400

    Add MainActivity.java

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,42 @@
+
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        
+        
+        
+        myWebView.setWebViewClient(new WebViewClient());
+        
+        // Load the local asset file (index.html)
+        myWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
